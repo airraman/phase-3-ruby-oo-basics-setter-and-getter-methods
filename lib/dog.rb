e6818caedf60f232e4e010b9804d8e16b252b947
Raman <ramanmcreates@gmail.com>
2022-11-22 13:32:44 -0500

    commit tree 3af77bfec828bb06c4e79615ff8799ecaca993dc

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,25 @@
+require 'pry'
+
 class Dog
 
+    def name=(nameinput)
+        @name = nameinput
+    end
+
+    def breed=(breedinput)
+        @breed = breedinput
+    end
+
+    def name
+        @name
+    end
+
+    def breed
+        @breed
+    end
+
+  
+
 end
+
+leo = Dog.new
